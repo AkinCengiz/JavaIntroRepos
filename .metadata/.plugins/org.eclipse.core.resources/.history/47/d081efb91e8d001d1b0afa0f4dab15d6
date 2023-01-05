26799223afd28ccc086230a6c11d062f3b4fa71a
akincengiz <akincengiz@outlook.com.tr>
2023-01-05 22:05:39 +0300

    Point ve PointTest classlari yuklendi...

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/47/d081efb91e8d001d1b0afa0f4dab15d6
@@ -0,0 +1,11 @@
+package org.akincengiz.university;
+
+public class Professor {
+	String no;
+	String name;
+	String dateOfBirth;
+	String rank;
+	Department department;
+	Course[] coursesGiven;
+	Student[] advisee;	
+}
--- Algorithms/src/org/akincengiz/university/Student.java
+++ Algorithms/src/org/akincengiz/university/Student.java
@@ -7,4 +7,41 @@ public class Student {
 	Department department;
 	Professor advisor;
 	Course[] coursesTaken;
+	
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	public Professor getAdvisor() {
+		return advisor;
+	}
+	public void setAdvisor(Professor advisor) {
+		this.advisor = advisor;
+	}
+	public Course[] getCoursesTaken() {
+		return coursesTaken;
+	}
+	public void setCoursesTaken(Course[] coursesTaken) {
+		this.coursesTaken = coursesTaken;
+	}
 }
