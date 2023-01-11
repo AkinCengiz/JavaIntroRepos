8fdeaf821babe489b18b4eb3e848159ede143837
akincengiz <akincengiz@outlook.com.tr>
2023-01-11 19:01:15 +0300

    CommandLine klasoru yuklendi.

--- Algorithms/src/org/akincengiz/bookshelf/Author.java
+++ Algorithms/src/org/akincengiz/bookshelf/Author.java
@@ -1,21 +1,35 @@
 package org.akincengiz.bookshelf;
 
 public class Author {
-	String name;
+	String firstName;
+	String lastName;
 
-	public String getName() {
-		return name;
+	public String getLastName() {
+		return lastName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 	public Author()
 	{
-		this("");
+		this(null,null);
 	}
-	public Author(String name)
+	public Author(String lastName)
 	{
-		this.name = name;
+		this(null,lastName);
+	}
+	public Author(String firstName, String lastName)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
 	}
 }
---
+++ CommandLine/Test.java
@@ -0,0 +1,11 @@
+package u.v;
+import x.y.B;
+import t.s.C;
+import m.n.D;
+
+public class Test
+{
+public static void main(String[] args){
+	A a;
+}
+}
